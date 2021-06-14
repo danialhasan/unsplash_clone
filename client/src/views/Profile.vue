32cacd4fbb83f8d93b595e46995631382985dfe1
Danial Hasan <danialhasan14@gmail.com>
2021-06-14 17:59:38 -0400

    fixed typo in code that was present in last commit

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -41,6 +41,6 @@ export default {
 </script>
 <template>
   <h1 class="font-bold my-24 text-center text-3xl">
-    Welcome, <span id="name">{{ this.profile.named }}</span>
+    Welcome, <span id="name">{{ this.profile.name }}</span>
   </h1>
 </template>
