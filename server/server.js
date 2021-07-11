b6ff0ae2deb574bc55a0d2e21b47266971475843
Danial Hasan <danialhasan14@gmail.com>
2021-07-10 23:19:28 -0400

    reverted last change

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
 
 // base route 
 app.get('/', (req, res) => {
-    res.send(`Hello! ${process.env.ACCESS_CONTROL_ALLOW_ORIGIN}`)
+    res.send(`Hello! This is the root handler for Danial's Unsplash Clone project.`)
 })
 // Defining route middleware
 app.use('/api', require('./routes/api'));
